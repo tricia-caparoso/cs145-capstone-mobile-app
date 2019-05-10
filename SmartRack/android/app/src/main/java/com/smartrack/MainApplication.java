144bd5cdbe3d88f353e0375ead44fbbb9feb6c99
Tricia Caparoso <tricia-caparoso@gmail.com>
2019-05-10 21:36:09 +0800

    Added logo

--- SmartRack/android/app/src/main/java/com/smartrack/MainApplication.java
+++ SmartRack/android/app/src/main/java/com/smartrack/MainApplication.java
@@ -3,6 +3,7 @@ package com.smartrack;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.amazonaws.RNAWSCognitoPackage;
 import com.wix.RNCameraKit.RNCameraKitPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNDeviceInfo(),
             new RNAWSCognitoPackage(),
             new RNCameraKitPackage()
       );
